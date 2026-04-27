9fffe31ca0324cfca4c5db80e92285a89ea7ae73
Veronika Burdelnaya <nika.burdelnaya@mail.ru>
2026-04-27 10:39:19 +0300

    DB_URL via computed field

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -15,13 +16,17 @@ class Settings(BaseSettings):
     DB_NAME: str
     DB_HOST: str
     DB_PORT: int = 5432
-    DB_URL: str
 
     RABBITMQ_URL: str
 
     API_PREFIX: str
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    @computed_field
+    @property
+    def DB_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 
 settings = Settings()
